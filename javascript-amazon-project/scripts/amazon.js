86db458bbfee2626fdd5f0a48962f1a4205e79be
SajadSoqrati <sajadsoqrati@gmail.com>
2024-11-28 19:51:43 +0330

    Create added message

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -42,7 +42,7 @@ products.forEach((product)=>{
 
             <div class="product-spacer"></div>
 
-            <div class="added-to-cart">
+            <div class="added-to-cart js-added-to-cart-${product.id}">
                 <img src="images/icons/checkmark.png">
                 Added
             </div>
@@ -60,15 +60,18 @@ innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
  .forEach((button)=>{
+    let addedMessageTimeoutId;
+
    button.addEventListener( 'click', ()=>{
      const {productId} = button.dataset;
+     
       let matchingItem; 
       cart.forEach((item)=>{
         if(productId === item.productId){
             matchingItem = item
         }
       });
-
+      
        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
        const quantity = Number(quantitySelector.value);
 
@@ -89,5 +92,15 @@ document.querySelectorAll('.js-add-to-cart')
        document.querySelector('.js-cart-quantity')
        .innerHTML = cartQuantity;
 
+       const addedMessage= document.querySelector(`.js-added-to-cart-${productId}`);
+        addedMessage.classList.add('added-to-cart-visible');
+        if(addedMessageTimeoutId){
+            clearTimeout(addedMessageTimeoutId)
+        }
+       
+        const timeoutId = setTimeout(()=>{
+            addedMessage.classList.remove('added-to-cart-visible')}
+        ,2000);
+        addedMessageTimeoutId = timeoutId;
   });
 });   
