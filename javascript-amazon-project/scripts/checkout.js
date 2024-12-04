1faaf9e7cb083c0519bde67df32996ee19f31109
SajadSoqrati <sajadsoqrati@gmail.com>
2024-12-04 19:10:35 +0330

    Add validation feature

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -142,11 +142,18 @@ cart.forEach((cartItem)=>{
 document.querySelectorAll('.js-save-link')
 .forEach((link)=>{link.addEventListener('click',()=>{
     const {productId} = link.dataset;
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    container.classList.remove('is-editing-quantity');
     const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
     const newQuantity = Number(quantityInput.value);
+    if(newQuantity < 0 || newQuantity >= 1000){
+        alert('Quantity must be at least 0 and less than 1000');
+        return;
+    }
     updateQuantity(productId, newQuantity);
+
+    const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+    );
+    container.classList.remove('is-editing-quantity');
 
     const quantityLabel = document.querySelector(
         `.js-quantity-label-${productId}`
