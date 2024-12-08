5a3a8a3967b886677b3cc5299bba38d94c99abaa
SajadSoqrati <sajadsoqrati@gmail.com>
2024-12-08 14:07:41 +0330

    Save data and generate HTML for payment summary

--- javascript-amazon-project/data/deliveryOptions.js
+++ javascript-amazon-project/data/deliveryOptions.js
@@ -13,3 +13,13 @@ deliveryDays:1 ,
 priceCents: 999
 }
 ];
+
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+    deliveryOptions.forEach((option)=>{
+        if(option.id === deliveryOptionId){
+            deliveryOption = option;
+        }
+    });
+    return deliveryOption || deliveryOptions[0];
+}
