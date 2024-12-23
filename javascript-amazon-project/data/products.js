0a9c96b8e961891b0c157b26af62732be5b5dcaa
SajadSoqrati <sajadsoqrati@gmail.com>
2024-12-23 16:51:45 +0330

    Convert product into classes

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
 class Product {
 
   id;
@@ -14,29 +16,18 @@ constructor(productDetails){
   this.rating =productDetails.rating;
   this.priceCents = productDetails.priceCents;
 }
+
+getStarsUrl(){
+  return `images/ratings/rating-${(this.rating.stars)*10}.png`;
+
 }
 
+getPrice(){
+  return `$${formatCurrency(this.priceCents )}`
 
-const product1 = new Product({ 
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]
-});
+}
 
-
-console.log(product1);
-
-
+}
 
 export function getProduct(productId){
   let matchingProduct;
