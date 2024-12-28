d68056b50d4477643b5ce7308b87b33b16a8a2c6
SajadSoqrati <sajadsoqrati@gmail.com>
2024-12-28 23:47:19 +0330

    Load products from the backend

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -76,7 +76,29 @@ export function getProduct(productId){
   });
   return matchingProduct;
 } 
+
+export let products = [];
+export function loadProducts (fun){
+ const xhr = new XMLHttpRequest();  
+xhr.addEventListener('load',()=>{
+products = JSON.parse(xhr.response).map((productDetails)=>{
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);  
+  } else if (productDetails.type === 'appliance'){
+    return new Appliance(productDetails);
+  }
+ return new Product(productDetails);
+});
+console.log('load products');
+
+fun();
+});
+
+ xhr.open('GET','https://supersimplebackend.dev/products');
+ xhr.send();  
+}
  
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -785,4 +807,4 @@ export const products = [
     return new Appliance(productDetails);
   }
  return new Product(productDetails);
-});
+});*/
